1c18cb379c02eb41a6705ae27e7f61b21f39bdb0
Jake Peterson <jake@acogdev.com>
2025-09-28 03:14:19 +0000

    add deplay script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Only allow specific commands
+case "$SSH_ORIGINAL_COMMAND" in
+    "deploy")
+	cd /home/jake/traefik/cq-bootstrap
+        git pull origin main
+        docker compose rm -f -s cq
+        docker compose up -d
+        #docker image prune -f
+        ;;
+    "status")
+        docker ps | grep cq || echo "Container not running"
+        ;;
+    "logs")
+        docker logs cq --tail 50
+        ;;
+    *)
+        echo "Command not allowed: $SSH_ORIGINAL_COMMAND"
+        exit 1
+        ;;
+esac
